a2786301804a3706fb7489cf31188cf3de093c2d
Mehedi Hasan <13490077+mhgitrepo@users.noreply.github.com>
2022-05-19 21:02:40 +0600

    pin generate: simpmlified version

--- js/pin-gen.js
+++ js/pin-gen.js
@@ -10,7 +10,42 @@ function getPin(){
 }
 function generatePin(){
     const pin = getPin();
-    console.log(pin);
+    // console.log(pin);
     
     document.getElementById('pin-gen-input').value = pin;
+}
+
+//type your pin
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    //check number or not
+    if(isNaN(number)){
+        // console.log(number);
+        if(number === 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
+
+//verify pin
+function verifyPin(){
+    const generatedPin = document.getElementById('pin-gen-input').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+
+    const verificationSuccess = document.getElementById('verification-success');
+    const verificationFail = document.getElementById('verification-fail');
+
+    if(generatedPin === typedNumbers){
+        verificationSuccess.style.display = 'block';
+        verificationFail.style.display = 'none';
+    } else{
+        verificationSuccess.style.display = 'none';
+        verificationFail.style.display = 'block';
+    }
 }
